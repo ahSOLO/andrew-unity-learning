cef160a06704c174fdcedc3a7e91a74733218ada
DauntlessDevelopment <82972094+DauntlessDevelopment@users.noreply.github.com>
2021-05-26 18:07:13 +0100

    Added basic structure

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : Agent
+{
+
+    private Entity current_target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(current_target == null)
+        {
+            Wander();
+        }
+    }
+
+    private void SearchForEnemy()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+
+        }
+    }
+
+    private void Wander()
+    {
+        
+    }
+}
