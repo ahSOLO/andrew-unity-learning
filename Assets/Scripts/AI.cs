993ff47b796c839bb9c9ba0f40e66be32360ef9a
ahSOLO <junmenghan@gmail.com>
2021-05-31 22:30:09 -0700

    implement basic movement and wander mechanic for AI

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class AI : Agent
 {
+    private float wanderDistance = 5f;
+    private float wanderTimer = 0f;
+    private float wanderTimerInterval = 2f;
 
     private Entity current_target;
     // Start is called before the first frame update
@@ -31,6 +34,13 @@ public class AI : Agent
 
     private void Wander()
     {
-        
+        wanderTimer -= Time.deltaTime;
+
+        if (wanderTimer <= 0)
+        {
+            // Using this method instead of insideUnitCircle because we want to randomize x and z instead of x and y.
+            destination = new Vector3(Random.value, 0, Random.value) * wanderDistance;
+            wanderTimer = wanderTimerInterval;
+        }
     }
 }
--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class Agent : Entity
 {
-    protected float movement_speed = 10f;
+    protected float movement_speed = 4f;
     protected float turn_speed = 270f;
+
+    protected Vector3 destination;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        destination = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        var diff = destination - transform.position;
+        var direction = diff.normalized;
+        Turn(direction);
+        if (diff.sqrMagnitude > 2)
+            Move(direction);
     }
 
     // Update is called once per frame
@@ -20,12 +32,23 @@ public class Agent : Entity
 
     protected void Move(Vector3 direction)
     {
-        
+        // Do not move vertically
+        // var moveDir = new Vector3(direction.x, 0f, direction.z) * movement_speed * Time.fixedDeltaTime;
+        // transform.Translate(moveDir, Space.World);
+
+        transform.Translate(transform.forward * movement_speed * Time.fixedDeltaTime, Space.World);
     }
 
-    protected void Turn(Vector3 rotation)
-    {
-
+    protected void Turn(Vector3 direction)
+    {        
+        var rotation = Quaternion.Euler(
+            transform.rotation.eulerAngles.x, 
+            // Only rotate on the Y axis
+            Mathf.MoveTowardsAngle(transform.rotation.eulerAngles.y, Quaternion.LookRotation(direction).eulerAngles.y, turn_speed * Time.fixedDeltaTime), 
+            transform.rotation.eulerAngles.z
+            );
+        
+        transform.rotation = rotation;
     }
 
     protected void UseItem()
